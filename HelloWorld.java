c9992de8794124025dc6dadcc408364e25d73a5f
toneloc <awklausing@gmail.com>
2016-06-01 12:14:07 -0500

    my first class is doen
    booyah

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+//this is my first java app :) woohoo and the rain stopped!
+
+public class HelloWorld {
+    //"In the beginning there was the Main," sayeth Lord Java
+    public static void main (String[] args) {
+        System.out.println("I'm ready to kick some Android ass.");
+        System.out.println("Wake up, code some app.");
+        System.out.println("If there's a bug, it shall be zapped.");
+        System.out.println("And when I compile, computers will clap.");
+        System.out.println("Every once in a while, take a nap.");
+    }
+}
