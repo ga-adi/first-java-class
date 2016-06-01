0578674bc68e26d1b6c9ff2cb1e0ef925c09dc8f
jackgt59 <TheDude@Jacks-MacBook-Air.local>
2016-06-01 12:14:07 -0500

    good stuff

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+// this is my first java app:)
+// Cardinals start soon
+public class HelloWorld {
+    //main== starting point
+    //all code begins here
+    //when we run our app...
+    //main() is ran first
+    public static void main(String[] args) {
+        System.out.println("I want to win");
+        System.out.println("Call me Mariano Rivera cause I'm a closer");
+        System.out.println("I am the best gambler in the entire world");
+        System.out.println("You think I'm kidding, I'm not. I am the best gambler in the street. I will steal yo money andclown your face");
+    }
+}
