39cb420f7fd9eb2ee769893472ba39fbf42b19ab
Jeff Chin <dizaburrow@gmail.com>
2016-06-01 12:14:25 -0500

    First Class is complete
    Boo Yah!!!!

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+// this is my first java app
+// and the rain stopped.
+public class HelloWorld  {
+    // main == starting pointing
+    // all code strts here
+    //when we run our app...
+    // main() is run first
+    public static void main(String[] args) {
+        System.out.println("Hi there.  There is something amiss...");
+        System.out.println("My pants were dry due to my awesome rain pants");
+        System.out.println("I'm not used to the rain since I've come from drought-stricken Cali");
+    }
+}
