5d21e7667b0167b1e4305a504cf6fc9286feeaad
code-for-coffee <jtraverjr@gmail.com>
2016-06-01 12:14:00 -0500

    my first class is done!
    huzzaahhhh!

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+// this is my first java app :)
+// and the rain stopped
+public class HelloWorld {
+    // main == starting point
+    // all code begins here
+    // when we run our app...
+    // main() is ran first
+    public static void main(String[] args) {
+        System.out.println("Hi there, I need beer.");
+        System.out.println("I was rained on today.");
+        System.out.println("I had a redbull.");
+        System.out.println("I have wings so peace out!");
+    }
+}
